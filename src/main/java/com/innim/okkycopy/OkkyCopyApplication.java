86f89a82e5d55169221eea4ccf20235d7c72db4b
5innim <bjm22247@gmail.com>
2023-08-31 00:42:34 +0900

    init project

---
+++ src/main/java/com/innim/okkycopy/OkkyCopyApplication.java
@@ -0,0 +1,13 @@
+package com.innim.okkycopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkkyCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkkyCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innim/okkycopy/OkkyCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innim.okkycopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkkyCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
