61f3b20efbc6081d518eaf7233b5252c767b27e9
5innim <bjm22247@gmail.com>
2023-09-14 15:04:55 +0900

    feat: Add handler method for /member/signup

---
+++ src/main/java/com/innim/okkycopy/domain/member/MemberController.java
@@ -0,0 +1,28 @@
+package com.innim.okkycopy.domain.member;
+
+import com.innim.okkycopy.domain.member.dto.request.SignupRequest;
+import com.innim.okkycopy.domain.member.dto.response.BriefMemberInfo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/member/signup")
+    public ResponseEntity<BriefMemberInfo> signup(@RequestBody SignupRequest signupRequest) {
+        log.trace("signup invoked");
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(memberService.insertMember(signupRequest));
+    }
+
+}
