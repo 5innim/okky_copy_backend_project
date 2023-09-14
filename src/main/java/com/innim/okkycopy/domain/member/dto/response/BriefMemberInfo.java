29b364fb0feebc432b885ec899ab6c3f8d36bc8c
5innim <bjm22247@gmail.com>
2023-09-14 15:03:35 +0900

    feat: Add dto for response

---
+++ src/main/java/com/innim/okkycopy/domain/member/dto/response/BriefMemberInfo.java
@@ -0,0 +1,15 @@
+package com.innim.okkycopy.domain.member.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class BriefMemberInfo {
+    private String nickname;
+    private String name;
+    private String email;
+    private String createdDate;
+}
