8c50da978dea478cfb6c8fc4edad0610b819895b
5innim <bjm22247@gmail.com>
2023-09-15 17:00:03 +0900

    feat: Add Member entity class

---
+++ src/main/java/com/innim/okkycopy/domain/member/entity/Member.java
@@ -0,0 +1,50 @@
+package com.innim.okkycopy.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "member")
+@DynamicUpdate
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, name = "email_check")
+    private boolean emailCheck;
+
+    @Column(nullable = false, name = "created_date")
+    private LocalDateTime createdDate;
+
+}
