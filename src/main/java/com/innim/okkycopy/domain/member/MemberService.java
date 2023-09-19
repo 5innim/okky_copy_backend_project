500ab2f87617ad9743831e957a8e1bae34b2f083
5innim <bjm22247@gmail.com>
2023-09-19 16:55:32 +0900

    feat: Add insert logic at MemberService class

--- src/main/java/com/innim/okkycopy/domain/member/MemberService.java
+++ src/main/java/com/innim/okkycopy/domain/member/MemberService.java
@@ -2,6 +2,7 @@ package com.innim.okkycopy.domain.member;
 
 import com.innim.okkycopy.domain.member.dto.request.SignupRequest;
 import com.innim.okkycopy.domain.member.dto.response.BriefMemberInfo;
+import com.innim.okkycopy.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,10 +14,22 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
 
     public BriefMemberInfo insertMember(SignupRequest signupRequest) {
         log.trace("insertMember");
         signupRequest.encodePassword(passwordEncoder);
+
+        Member member = Member.builder()
+            .id(signupRequest.getId())
+            .password(signupRequest.getPassword())
+            .email(signupRequest.getEmail())
+            .name(signupRequest.getName())
+            .nickname(signupRequest.getNickname())
+            .emailCheck(signupRequest.isEmailCheck())
+            .build();
+
+        memberRepository.save(member);
 
         log.trace(signupRequest.getPassword());
         return BriefMemberInfo.builder()
