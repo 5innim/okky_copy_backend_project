ac480e8fd7abc71071b32d7d0aa34fd4c10ef4a0
5innim <bjm22247@gmail.com>
2023-10-12 09:25:16 +0900

    feat: Add "sub" claim at token

--- src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
+++ src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
@@ -11,21 +11,22 @@ public class JwtUtil {
     public static String generateAccessToken(String userId) {
         Date date = new Date();
         date.setTime(date.getTime() + JwtProperty.accessValidTime);
-        return generateToken(userId, date);
+        return generateToken(userId, date, "access");
     }
 
     public static String generateRefreshToken(String userId) {
         Date date = new Date();
         date.setTime(date.getTime() + JwtProperty.refreshValidTime);
-        return generateToken(userId, date);
+        return generateToken(userId, date, "refresh");
     }
 
-    private static String generateToken(String userId, Date expiration) {
+    private static String generateToken(String userId, Date expiration, String tokenSub) {
 
         return Jwts.builder()
-            .setHeader(Map.of("alg", JwtProperty.algorithm, "typ", "jwt"))
-            .setClaims(Map.of("uid", userId))
+            .setHeader(Map.of("alg", JwtProperty.algorithm, "typ", "JWT"))
             .setExpiration(expiration)
+            .setSubject(tokenSub)
+            .addClaims(Map.of("uid", userId))
             .signWith(JwtProperty.secretKey, JwtProperty.signatureAlgorithm)
             .compact();
     }
