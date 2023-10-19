3f122056fc93138a310fec37af783b91f9f46569
5innim <bjm22247@gmail.com>
2023-10-19 11:01:54 +0900

    feat: Add error code for login api

--- src/main/java/com/innim/okkycopy/global/error/ErrorCode.java
+++ src/main/java/com/innim/okkycopy/global/error/ErrorCode.java
@@ -14,8 +14,11 @@ public enum ErrorCode {
     _400_INVALID_NAME (HttpStatus.BAD_REQUEST, "400004", "input value of 'name' is invalid"),
     _400_INVALID_NICKNAME (HttpStatus.BAD_REQUEST, "400005", "input value of 'nickname' is invalid"),
     _400_INVALID_UNEXPECTED (HttpStatus.BAD_REQUEST, "400100", "unexpected input is invalid"),
+    _400_BAD_CREDENTIALS (HttpStatus.BAD_REQUEST, "400011", "'id' or 'password' is incorrect"),
+    _400_AUTHENTICATION_FAILED (HttpStatus.BAD_REQUEST, "400012", "authentication is failed"),
     _409_DUPLICATE_ID (HttpStatus.CONFLICT, "409001", "input value of 'id' is duplicated"),
-    _409_DUPLICATE_EMAIL (HttpStatus.CONFLICT, "409002", "input value of 'email' is duplicated");
+    _409_DUPLICATE_EMAIL (HttpStatus.CONFLICT, "409002", "input value of 'email' is duplicated"),
+    _500_GENERATE_TOKEN (HttpStatus.INTERNAL_SERVER_ERROR, "500001", "can not generate token");
 
     private final HttpStatus status;
     private final String code;
