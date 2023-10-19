065b3bbe6a587ca1acaf630a2fd5a6b417d4e9f0
5innim <bjm22247@gmail.com>
2023-10-19 11:05:15 +0900

    fix: Fix input type for methods at JwtUtil class

--- src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
+++ src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
@@ -8,19 +8,19 @@ import java.util.Map;
 
 public class JwtUtil {
 
-    public static String generateAccessToken(String userId) {
+    public static String generateAccessToken(Long userId) throws TokenGenerateException {
         Date date = new Date();
         date.setTime(date.getTime() + JwtProperty.accessValidTime);
         return generateToken(userId, date, "access");
     }
 
-    public static String generateRefreshToken(String userId) {
+    public static String generateRefreshToken(Long userId) throws TokenGenerateException {
         Date date = new Date();
         date.setTime(date.getTime() + JwtProperty.refreshValidTime);
         return generateToken(userId, date, "refresh");
     }
 
-    private static String generateToken(String userId, Date expiration, String tokenSub) {
+    private static String generateToken(Long userId, Date expiration, String tokenSub) throws TokenGenerateException{
 
         return Jwts.builder()
             .setHeader(Map.of("alg", JwtProperty.algorithm, "typ", "JWT"))
