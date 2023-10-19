81a673e26e314f9e612c13886071a959ea679e58
5innim <bjm22247@gmail.com>
2023-10-19 11:03:30 +0900

    feat: Add exception class for token generation

---
+++ src/main/java/com/innim/okkycopy/global/error/exception/TokenGenerateException.java
@@ -0,0 +1,9 @@
+package com.innim.okkycopy.global.error.exception;
+
+public class TokenGenerateException extends Exception {
+
+    public TokenGenerateException(String message) {
+        super(message);
+    }
+
+}
