4568b8b7b934d51a9611e962cd810849cd9f52a7
5innim <bjm22247@gmail.com>
2023-10-19 11:06:18 +0900

    fix: Add exception handling logic at generateToken method

--- src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
+++ src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
@@ -1,11 +1,14 @@
 package com.innim.okkycopy.global.util;
 
 
+import com.innim.okkycopy.global.error.exception.TokenGenerateException;
 import com.innim.okkycopy.global.util.property.JwtProperty;
 import io.jsonwebtoken.Jwts;
 import java.util.Date;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class JwtUtil {
 
     public static String generateAccessToken(Long userId) throws TokenGenerateException {
@@ -22,13 +25,21 @@ public class JwtUtil {
 
     private static String generateToken(Long userId, Date expiration, String tokenSub) throws TokenGenerateException{
 
-        return Jwts.builder()
-            .setHeader(Map.of("alg", JwtProperty.algorithm, "typ", "JWT"))
-            .setExpiration(expiration)
-            .setSubject(tokenSub)
-            .addClaims(Map.of("uid", userId))
-            .signWith(JwtProperty.secretKey, JwtProperty.signatureAlgorithm)
-            .compact();
+        String generatedToken;
+        try {
+            generatedToken = Jwts.builder()
+                .setHeader(Map.of("alg", JwtProperty.algorithm, "typ", "JWT"))
+                .setExpiration(expiration)
+                .setSubject(tokenSub)
+                .addClaims(Map.of("uid", userId))
+                .signWith(JwtProperty.secretKey, JwtProperty.signatureAlgorithm)
+                .compact();
+        } catch(Exception ex) {
+            log.info(ex.getMessage());
+            throw new TokenGenerateException("can not generate token with userId: " + "[" + userId + "]");
+        }
+
+        return generatedToken;
     }
 
 
