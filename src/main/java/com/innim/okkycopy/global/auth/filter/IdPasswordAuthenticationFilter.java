afdcc58604631f28bc00899c02ed3708e269a5bb
5innim <bjm22247@gmail.com>
2023-10-19 11:07:36 +0900

    feat: Add logic for result handling for authentication

--- src/main/java/com/innim/okkycopy/global/auth/filter/IdPasswordAuthenticationFilter.java
+++ src/main/java/com/innim/okkycopy/global/auth/filter/IdPasswordAuthenticationFilter.java
@@ -1,9 +1,13 @@
 package com.innim.okkycopy.global.auth.filter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.innim.okkycopy.global.auth.dto.LoginRequest;
+import com.innim.okkycopy.global.auth.CustomUserDetails;
+import com.innim.okkycopy.global.auth.dto.request.LoginRequest;
+import com.innim.okkycopy.global.auth.dto.response.LoginResponse;
+import com.innim.okkycopy.global.error.ErrorCode;
+import com.innim.okkycopy.global.error.exception.TokenGenerateException;
+import com.innim.okkycopy.global.util.JwtUtil;
+import com.innim.okkycopy.global.util.RequestResponseUtil;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -19,13 +24,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Slf4j
 public class IdPasswordAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-
-    public static final String SPRING_SECURITY_FORM_ID_KEY = "id";
-    public static final String SPRING_SECURITY_FORM_PASSWORD_KEY = "password";
     private static final AntPathRequestMatcher DEFAULT_ANT_PATH_REQUEST_MATCHER = new AntPathRequestMatcher("/login",
         "POST");
-    private String idParameter = SPRING_SECURITY_FORM_ID_KEY;
-    private String passwordParameter = SPRING_SECURITY_FORM_PASSWORD_KEY;
     private boolean postOnly = true;
 
     public IdPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -35,7 +35,7 @@ public class IdPasswordAuthenticationFilter extends AbstractAuthenticationProces
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
         HttpServletResponse response)
-        throws AuthenticationException, IOException, ServletException {
+        throws AuthenticationException, IOException {
         if (this.postOnly && !request.getMethod().equals("POST")) {
             throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
         }
@@ -53,19 +53,40 @@ public class IdPasswordAuthenticationFilter extends AbstractAuthenticationProces
 
     @Nullable
     private LoginRequest obtainBody(HttpServletRequest request) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(request.getInputStream(), LoginRequest.class);
+        return RequestResponseUtil.jsonToObject(request.getInputStream(), LoginRequest.class);
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-        Authentication authResult) throws IOException, ServletException {
-        log.debug("Authentication is success");
+        Authentication authResult) throws IOException {
+
+        CustomUserDetails userDetails = (CustomUserDetails) authResult.getPrincipal();
+        long userId = userDetails.getUserId();
+
+        LoginResponse body = new LoginResponse();
+        try {
+            body.setAccessToken(JwtUtil.generateAccessToken(userId));
+            body.setRefreshToken(JwtUtil.generateRefreshToken(userId));
+        } catch(TokenGenerateException ex) {
+            RequestResponseUtil.makeExceptionResponseForFilter(response,
+                ErrorCode._500_GENERATE_TOKEN);
+            return;
+        }
+
+        RequestResponseUtil.makeJsonResponseHeader(response);
+        response.getWriter().write(RequestResponseUtil.objectToJson(body));
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-        AuthenticationException failed) throws IOException, ServletException {
+        AuthenticationException failed) throws IOException {
+
+        if (failed instanceof BadCredentialsException) {
+            RequestResponseUtil.makeExceptionResponseForFilter(response, ErrorCode._400_BAD_CREDENTIALS);
+            return;
+        }
+
+        RequestResponseUtil.makeExceptionResponseForFilter(response, ErrorCode._400_AUTHENTICATION_FAILED);
 
     }
 }
