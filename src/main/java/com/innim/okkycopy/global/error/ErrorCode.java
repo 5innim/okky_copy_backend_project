2b1a100b2886c6ff84d62ccd4ad58410e04c65c6
5innim <bjm22247@gmail.com>
2023-10-23 15:01:00 +0900

    feat: Add error code cases

--- src/main/java/com/innim/okkycopy/global/error/ErrorCode.java
+++ src/main/java/com/innim/okkycopy/global/error/ErrorCode.java
@@ -16,6 +16,9 @@ public enum ErrorCode {
     _400_INVALID_UNEXPECTED (HttpStatus.BAD_REQUEST, "400100", "unexpected input is invalid"),
     _400_BAD_CREDENTIALS (HttpStatus.BAD_REQUEST, "400011", "'id' or 'password' is incorrect"),
     _400_AUTHENTICATION_FAILED (HttpStatus.BAD_REQUEST, "400012", "authentication is failed"),
+    _400_INVALID_TOKEN_VALUE(HttpStatus.BAD_REQUEST, "400013", "invalid token value"),
+    _401_TOKEN_AUTHENTICATION_FAIL(HttpStatus.UNAUTHORIZED, "401001", "this token is not valid"),
+    _403_TOKEN_EXPIRED (HttpStatus.FORBIDDEN, "403001", "token is expired"),
     _409_DUPLICATE_ID (HttpStatus.CONFLICT, "409001", "input value of 'id' is duplicated"),
     _409_DUPLICATE_EMAIL (HttpStatus.CONFLICT, "409002", "input value of 'email' is duplicated"),
     _500_GENERATE_TOKEN (HttpStatus.INTERNAL_SERVER_ERROR, "500001", "can not generate token");
