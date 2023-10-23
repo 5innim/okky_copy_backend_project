812fc69b818cddbc270c27b4812dc849dbfbb993
5innim <bjm22247@gmail.com>
2023-10-23 14:56:58 +0900

    feat: Add Exceptions for token validation

---
+++ src/main/java/com/innim/okkycopy/global/error/exception/FailValidationJwtException.java
@@ -0,0 +1,7 @@
+package com.innim.okkycopy.global.error.exception;
+
+public class FailValidationJwtException extends RuntimeException {
+    public FailValidationJwtException(String message) {
+        super(message);
+    }
+}
