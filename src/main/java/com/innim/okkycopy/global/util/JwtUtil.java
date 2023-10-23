c7e192c8d4b4b4e7b45e27e263683641a37dc57c
5innim <bjm22247@gmail.com>
2023-10-23 14:55:50 +0900

    feat: Add method for token validation

--- src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
+++ src/main/java/com/innim/okkycopy/global/util/JwtUtil.java
@@ -1,9 +1,15 @@
 package com.innim.okkycopy.global.util;
 
 
+import com.innim.okkycopy.global.error.exception.FailValidationJwtException;
 import com.innim.okkycopy.global.error.exception.TokenGenerateException;
 import com.innim.okkycopy.global.util.property.JwtProperty;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.SignatureException;
 import java.util.Date;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
@@ -42,6 +48,19 @@ public class JwtUtil {
         return generatedToken;
     }
 
+    public static Claims validateToken(String token) throws ExpiredJwtException, FailValidationJwtException {
 
+        try {
+            JwtParser parser = Jwts.parserBuilder().setSigningKey(JwtProperty.secretKey).build();
+            return parser.parseClaimsJws(token).getBody();
+        } catch(ExpiredJwtException ex) {
+            throw ex;
+        } catch(SignatureException ex) {
+            throw new FailValidationJwtException("signature is not equals");
+        } catch(JwtException ex) {
+            throw new FailValidationJwtException("token validation failed");
+        }
+
+    }
     
 }
