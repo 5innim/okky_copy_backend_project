9979714c32321f8f724cc0c1a57bbed2b76781c2
5innim <bjm22247@gmail.com>
2023-10-31 09:44:15 +0900

    feat: Add RefreshResponse dto for /refresh request

---
+++ src/main/java/com/innim/okkycopy/global/auth/dto/response/RefreshResponse.java
@@ -0,0 +1,10 @@
+package com.innim.okkycopy.global.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RefreshResponse {
+    private String accessToken;
+}
