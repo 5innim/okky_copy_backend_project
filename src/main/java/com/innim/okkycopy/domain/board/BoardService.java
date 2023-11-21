2383bdb2a504a464ede188fe6f40578623b82249
5innim <bjm22247@gmail.com>
2023-11-21 13:36:38 +0900

    feat: Add exception logic for /board/topics

--- src/main/java/com/innim/okkycopy/domain/board/BoardService.java
+++ src/main/java/com/innim/okkycopy/domain/board/BoardService.java
@@ -3,6 +3,8 @@ package com.innim.okkycopy.domain.board;
 import com.innim.okkycopy.domain.board.dto.response.topics.TopicsResponse;
 import com.innim.okkycopy.domain.board.entity.BoardType;
 import com.innim.okkycopy.domain.board.repository.BoardTypeRepository;
+import com.innim.okkycopy.global.error.ErrorCode;
+import com.innim.okkycopy.global.error.exception.FailInitializationException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,10 @@ public class BoardService {
 
     public TopicsResponse findAllBoardTypes() {
         List<BoardType> boardTypes = boardTypeRepository.findAll();
+
+        if (boardTypes.isEmpty())
+            throw new FailInitializationException(ErrorCode._500_FAIL_INITIALIZATION);
+
         return TopicsResponse.toDto(boardTypes);
     }
 
