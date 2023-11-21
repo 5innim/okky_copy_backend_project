fb1fbcf241fa5156a24f6007ddc2e23807bda624
5innim <bjm22247@gmail.com>
2023-11-21 13:15:00 +0900

    feat: Add RequestMapping annotation for request mapping

--- src/main/java/com/innim/okkycopy/domain/board/BoardController.java
+++ src/main/java/com/innim/okkycopy/domain/board/BoardController.java
@@ -5,14 +5,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
-    @GetMapping("/board/topics")
+    @GetMapping("/topics")
     public ResponseEntity serveTopics (HttpServletRequest request, HttpServletResponse response) {
 
         return ResponseEntity.ok(boardService.findAllBoardTypes());
