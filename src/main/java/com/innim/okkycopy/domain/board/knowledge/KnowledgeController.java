0e1c1912df5abc559ac71acd7f500971cfee1c98
5innim <bjm22247@gmail.com>
2023-11-27 16:04:47 +0900

    feat: Add handler method for /board/knowledge/posts/{id}

--- src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeController.java
+++ src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,12 @@ public class KnowledgeController {
         knowledgeService.saveKnowledgePost(writeRequest, customUserDetails);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Object> getKnowledgePost(@PathVariable("id") long id) {
+
+        return ResponseEntity.ok(knowledgeService.selectKnowledgePost(id));
     }
 
 }
