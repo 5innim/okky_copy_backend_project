cfd0e78de7de8d2a79d73f1dcc6839f4a45b648a
5innim <bjm22247@gmail.com>
2023-12-17 15:17:51 +0900

    feat: Fix scraping logic for change of entities

--- src/main/java/com/innim/okkycopy/domain/board/BoardService.java
+++ src/main/java/com/innim/okkycopy/domain/board/BoardService.java
@@ -3,12 +3,15 @@ package com.innim.okkycopy.domain.board;
 import com.innim.okkycopy.domain.board.dto.response.topics.TopicsResponse;
 import com.innim.okkycopy.domain.board.entity.BoardType;
 import com.innim.okkycopy.domain.board.entity.Post;
+import com.innim.okkycopy.domain.board.entity.Scrap;
 import com.innim.okkycopy.domain.board.repository.BoardTypeRepository;
 import com.innim.okkycopy.domain.board.repository.PostRepository;
+import com.innim.okkycopy.domain.board.repository.ScrapRepository;
 import com.innim.okkycopy.domain.member.entity.Member;
 import com.innim.okkycopy.global.error.ErrorCode;
 import com.innim.okkycopy.global.error.exception.FailInitializationException;
 import com.innim.okkycopy.global.error.exception.NoSuchPostException;
+import com.innim.okkycopy.global.error.exception.NoSuchScrapException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
@@ -21,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class BoardService {
     private final BoardTypeRepository boardTypeRepository;
     private final PostRepository postRepository;
+    private final ScrapRepository scrapRepository;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -37,17 +41,21 @@ public class BoardService {
     @Transactional
     public void scrapPost(Member member, long postId) {
         Member mergedMember = entityManager.merge(member);
-
         Post post = postRepository.findByPostId(postId).orElseThrow(() -> new NoSuchPostException(ErrorCode._400_NO_SUCH_POST));
-        mergedMember.getScrappedPosts().add(post);
+        Scrap scrap = Scrap.builder()
+            .post(post)
+            .member(mergedMember)
+            .build();
+
+        entityManager.persist(scrap);
     }
 
     @Transactional
     public void cancelScrap(Member member, long postId) {
         Member mergedMember = entityManager.merge(member);
-
         Post post = postRepository.findByPostId(postId).orElseThrow(() -> new NoSuchPostException(ErrorCode._400_NO_SUCH_POST));
-        mergedMember.getScrappedPosts().remove(post);
+        Scrap scrap = scrapRepository.findByMemberAndPost(post, mergedMember).orElseThrow(() -> new NoSuchScrapException(ErrorCode._400_NO_SUCH_SCRAP));
+        entityManager.remove(scrap);
     }
 
 }
