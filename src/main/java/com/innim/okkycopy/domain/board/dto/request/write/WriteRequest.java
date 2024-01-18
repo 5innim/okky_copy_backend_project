ccd0edec7fed66c56ba483b3b2a89988538925bb
5innim <bjm22247@gmail.com>
2024-01-18 13:22:16 +0900

    feat: Add constructor and builder

--- src/main/java/com/innim/okkycopy/domain/board/dto/request/write/WriteRequest.java
+++ src/main/java/com/innim/okkycopy/domain/board/dto/request/write/WriteRequest.java
@@ -4,6 +4,8 @@ import com.innim.okkycopy.global.commons.validation.annotation.ListSize;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +13,8 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class WriteRequest {
     @Size(min = 1)
     private String topic;
