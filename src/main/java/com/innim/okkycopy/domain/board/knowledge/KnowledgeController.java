158d4f59f30c1ba8068d46befac903d885cdcead
5innim <bjm22247@gmail.com>
2024-02-06 10:54:44 +0900

    feat: Add handler method for [delete] /board/knowledge/posts/{id}

--- src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeController.java
+++ src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,14 @@ public class KnowledgeController {
         @RequestBody @Valid WriteRequest updateRequest,
         @PathVariable("id") long id) {
         knowledgeService.updateKnowledgePost(customUserDetails, updateRequest, id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Object> deleteKnowledgePost(
+        @AuthenticationPrincipal CustomUserDetails customUserDetails,
+        @PathVariable("id") long id) {
+        knowledgeService.deleteKnowledgePost(customUserDetails, id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
