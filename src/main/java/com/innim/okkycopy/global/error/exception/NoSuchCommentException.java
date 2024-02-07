34d153173c75233199590d63f407324ce251f585
5innim <bjm22247@gmail.com>
2024-02-07 14:52:16 +0900

    feat: Add error case for not exist comment

---
+++ src/main/java/com/innim/okkycopy/global/error/exception/NoSuchCommentException.java
@@ -0,0 +1,7 @@
+package com.innim.okkycopy.global.error.exception;
+
+import com.innim.okkycopy.global.error.ErrorCode;
+
+public class NoSuchCommentException extends ServiceException {
+    public NoSuchCommentException(ErrorCode errorCode) { super(errorCode); }
+}
