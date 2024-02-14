0f4a799b9e8291d2304672304b661e90a4b1cfb0
5innim <bjm22247@gmail.com>
2024-02-14 16:11:14 +0900

    feat: Add createKnowledgeReComment method at KnowledgeComment entity

--- src/main/java/com/innim/okkycopy/domain/board/knowledge/entity/KnowledgeComment.java
+++ src/main/java/com/innim/okkycopy/domain/board/knowledge/entity/KnowledgeComment.java
@@ -1,6 +1,7 @@
 package com.innim.okkycopy.domain.board.knowledge.entity;
 
 import com.innim.okkycopy.domain.board.dto.request.WriteCommentRequest;
+import com.innim.okkycopy.domain.board.dto.request.WriteReCommentRequest;
 import com.innim.okkycopy.domain.board.entity.Comment;
 import com.innim.okkycopy.domain.board.entity.Post;
 import com.innim.okkycopy.domain.member.entity.Member;
@@ -38,10 +39,24 @@ public class KnowledgeComment extends Comment {
     public static KnowledgeComment createKnowledgeComment(Post post, Member member, WriteCommentRequest writeCommentRequest) {
         KnowledgeComment comment = KnowledgeComment.builder()
             .content(writeCommentRequest.getContent())
+            .post(post)
+            .member(member)
+            .likes(0l)
             .build();
-        comment.setPost(post);
-        comment.setMember(member);
-        comment.setLikes(0l);
+
+        return comment;
+    }
+
+    public static KnowledgeComment createKnowledgeReComment(Post post, Member member, long parentId, WriteReCommentRequest writeReCommentRequest) {
+        KnowledgeComment comment = KnowledgeComment.builder()
+            .content(writeReCommentRequest.getContent())
+            .mentionedMember(writeReCommentRequest.getMentionId())
+            .parentId(parentId)
+            .post(post)
+            .member(member)
+            .likes(0l)
+            .build();
+
         return comment;
     }
 }
