9926800d935c6a1dd64cd52d9a1bc72a586a7179
5innim <bjm22247@gmail.com>
2024-02-14 17:39:27 +0900

    feat: Add annotations at WriteReCommentRequest class

--- src/main/java/com/innim/okkycopy/domain/board/dto/request/WriteReCommentRequest.java
+++ src/main/java/com/innim/okkycopy/domain/board/dto/request/WriteReCommentRequest.java
@@ -2,11 +2,17 @@ package com.innim.okkycopy.domain.board.dto.request;
 
 import com.innim.okkycopy.global.commons.validation.annotation.MentionId;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WriteReCommentRequest {
     @Size(min = 1, max = 20000)
     private String content;
