94b87e201eb7eb10ebcb6953f05d1253947b92d9
5innim <bjm22247@gmail.com>
2024-02-14 09:39:27 +0900

    feat: Add dto for request: [get]/board/---/posts/{id}/comments

---
+++ src/main/java/com/innim/okkycopy/domain/board/dto/response/comments/CommentResponse.java
@@ -0,0 +1,31 @@
+package com.innim.okkycopy.domain.board.dto.response.comments;
+
+import com.innim.okkycopy.domain.board.dto.response.post.detail.WriterInfoResponse;
+import com.innim.okkycopy.domain.board.entity.Comment;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class CommentResponse {
+    private WriterInfoResponse writerInfoResponse;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdate;
+    private Long likes;
+
+    public static CommentResponse toCommentResponseDto(Comment comment, String content, long likes) {
+        return CommentResponse.builder()
+            .writerInfoResponse((comment.getMember()==null) ? null:WriterInfoResponse.toWriterInfoRequestDto(comment.getMember()))
+            .content(content)
+            .createdDate(comment.getCreatedDate())
+            .lastUpdate(comment.getLastUpdate())
+            .likes(likes)
+            .build();
+    }
+}
