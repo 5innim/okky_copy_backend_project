1447d949ea6476754935cbb7863cf5bddd2fffc3
5innim <bjm22247@gmail.com>
2024-02-19 16:18:46 +0900

    feat: Define findByParentId method at CommentRepository interface

--- src/main/java/com/innim/okkycopy/domain/board/comment/CommentRepository.java
+++ src/main/java/com/innim/okkycopy/domain/board/comment/CommentRepository.java
@@ -2,8 +2,10 @@ package com.innim.okkycopy.domain.board.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findByCommentId(long commentId);
+    List<Comment> findByParentId(long parentId);
 }
