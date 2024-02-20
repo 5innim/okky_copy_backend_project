35d7b087b40d937672a8782121fdc39b64d5931b
5innim <bjm22247@gmail.com>
2024-02-20 15:17:24 +0900

    feat: Add logic for making requester info dto at selectKnowledgePost method

--- src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeService.java
+++ src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeService.java
@@ -2,10 +2,14 @@ package com.innim.okkycopy.domain.board.knowledge;
 
 import com.innim.okkycopy.domain.board.dto.request.write.WriteRequest;
 import com.innim.okkycopy.domain.board.dto.response.post.detail.PostDetailResponse;
+import com.innim.okkycopy.domain.board.dto.response.post.detail.PostRequesterInfoResponse;
 import com.innim.okkycopy.domain.board.entity.BoardTopic;
 import com.innim.okkycopy.domain.board.knowledge.entity.KnowledgePost;
 import com.innim.okkycopy.domain.board.knowledge.repository.KnowledgePostRepository;
 import com.innim.okkycopy.domain.board.repository.BoardTopicRepository;
+import com.innim.okkycopy.domain.board.repository.PostHateRepository;
+import com.innim.okkycopy.domain.board.repository.PostLikeRepository;
+import com.innim.okkycopy.domain.board.repository.ScrapRepository;
 import com.innim.okkycopy.domain.member.MemberRepository;
 import com.innim.okkycopy.domain.member.entity.Member;
 import com.innim.okkycopy.global.auth.CustomUserDetails;
@@ -26,6 +30,9 @@ public class KnowledgeService {
     private final BoardTopicRepository boardTopicRepository;
     private final KnowledgePostRepository knowledgePostRepository;
     private final MemberRepository memberRepository;
+    private final ScrapRepository scrapRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final PostHateRepository postHateRepository;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -41,12 +48,24 @@ public class KnowledgeService {
         entityManager.persist(knowledgePost);
     }
 
-    public PostDetailResponse selectKnowledgePost(long postId) {
+    public PostDetailResponse selectKnowledgePost(CustomUserDetails customUserDetails, long postId) {
         KnowledgePost knowledgePost = knowledgePostRepository.findByPostId(postId).orElseThrow(() -> new NoSuchPostException(ErrorCode._400_NO_SUCH_POST));
-        Member member = memberRepository.findByMemberId(knowledgePost.getMember().getMemberId()).orElseGet(() -> Member.builder().memberId(0l)
+        Member member = memberRepository.findByMemberId(knowledgePost.getMember().getMemberId()).orElseGet(() -> Member.builder().memberId(0L)
             .build());
 
-        return PostDetailResponse.toPostDetailResponseDto(knowledgePost, member);
+        PostDetailResponse response = PostDetailResponse.toPostDetailResponseDto(knowledgePost, member);
+        if (customUserDetails != null) {
+            Member requester = customUserDetails.getMember();
+            response.setPostRequesterInfoResponse(
+                    PostRequesterInfoResponse.builder()
+                            .isScraped(scrapRepository.findByMemberAndPost(knowledgePost, requester).isPresent())
+                            .isLiked(postLikeRepository.findByMemberAndPost(knowledgePost, requester).isPresent())
+                            .isHated(postHateRepository.findByMemberAndPost(knowledgePost, requester).isPresent())
+                            .build()
+            );
+        }
+
+        return response;
     }
 
     @Transactional
