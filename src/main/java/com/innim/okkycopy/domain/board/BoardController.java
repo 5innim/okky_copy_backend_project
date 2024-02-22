f40a3c8c3f4fd78ae17d28f26eaccae6bc8f91cc
5innim <bjm22247@gmail.com>
2024-02-22 14:36:06 +0900

    feat: Add handler method for [post] /posts/{id}/[like | hate]

--- src/main/java/com/innim/okkycopy/domain/board/BoardController.java
+++ src/main/java/com/innim/okkycopy/domain/board/BoardController.java
@@ -1,16 +1,12 @@
 package com.innim.okkycopy.domain.board;
 
 import com.innim.okkycopy.domain.board.dto.request.ScrapRequest;
+import com.innim.okkycopy.domain.board.enums.ExpressionType;
 import com.innim.okkycopy.global.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/board")
@@ -19,20 +15,34 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/topics")
-    public ResponseEntity serveTopics() {
+    public ResponseEntity<Object> serveTopics() {
         return ResponseEntity.ok(boardService.findAllBoardTopics());
     }
 
     @PostMapping("/post/scrap")
-    public ResponseEntity doScrap(@RequestBody ScrapRequest scrapRequest, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public ResponseEntity<Object> doScrap(@RequestBody ScrapRequest scrapRequest, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         boardService.scrapPost(customUserDetails.getMember(), scrapRequest.getPostId());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/post/scrap")
-    public ResponseEntity cancelScrap(@RequestBody ScrapRequest scrapRequest, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public ResponseEntity<Object> cancelScrap(@RequestBody ScrapRequest scrapRequest, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         boardService.cancelScrap(customUserDetails.getMember(), scrapRequest.getPostId());
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/posts/{id}/like")
+    public ResponseEntity<Object> makeLikeExpression(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable long id) {
+        boardService.insertPostExpression(customUserDetails.getMember(), id, ExpressionType.LIKE);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/posts/{id}/hate")
+    public ResponseEntity<Object> makeHateExpression(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable long id) {
+        boardService.insertPostExpression(customUserDetails.getMember(), id, ExpressionType.HATE);
+        return ResponseEntity.ok().build();
+    }
+
+
 
 }
