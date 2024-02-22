53bf668dab265e11b9857d0114b3645057d27aaf
5innim <bjm22247@gmail.com>
2024-02-22 14:42:54 +0900

    feat: Add insertPostExpression method at BoardService class

--- src/main/java/com/innim/okkycopy/domain/board/BoardService.java
+++ src/main/java/com/innim/okkycopy/domain/board/BoardService.java
@@ -3,12 +3,16 @@ package com.innim.okkycopy.domain.board;
 import com.innim.okkycopy.domain.board.dto.response.topics.TopicsResponse;
 import com.innim.okkycopy.domain.board.entity.BoardType;
 import com.innim.okkycopy.domain.board.entity.Post;
+import com.innim.okkycopy.domain.board.entity.PostExpression;
 import com.innim.okkycopy.domain.board.entity.Scrap;
+import com.innim.okkycopy.domain.board.enums.ExpressionType;
 import com.innim.okkycopy.domain.board.repository.BoardTypeRepository;
+import com.innim.okkycopy.domain.board.repository.PostExpressionRepository;
 import com.innim.okkycopy.domain.board.repository.PostRepository;
 import com.innim.okkycopy.domain.board.repository.ScrapRepository;
 import com.innim.okkycopy.domain.member.entity.Member;
 import com.innim.okkycopy.global.error.ErrorCode;
+import com.innim.okkycopy.global.error.exception.AlreadyExistExpressionException;
 import com.innim.okkycopy.global.error.exception.FailInitializationException;
 import com.innim.okkycopy.global.error.exception.NoSuchPostException;
 import com.innim.okkycopy.global.error.exception.NoSuchScrapException;
@@ -25,6 +29,7 @@ public class BoardService {
     private final BoardTypeRepository boardTypeRepository;
     private final PostRepository postRepository;
     private final ScrapRepository scrapRepository;
+    private final PostExpressionRepository postExpressionRepository;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -56,6 +61,16 @@ public class BoardService {
         Post post = postRepository.findByPostId(postId).orElseThrow(() -> new NoSuchPostException(ErrorCode._400_NO_SUCH_POST));
         Scrap scrap = scrapRepository.findByMemberAndPost(post, mergedMember).orElseThrow(() -> new NoSuchScrapException(ErrorCode._400_NO_SUCH_SCRAP));
         entityManager.remove(scrap);
+    }
+
+    @Transactional
+    public void insertPostExpression(Member member, long postId, ExpressionType type) {
+        Member mergedMember = entityManager.merge(member);
+        Post post = postRepository.findByPostId(postId).orElseThrow(() -> new NoSuchPostException(ErrorCode._400_NO_SUCH_POST));
+        if (postExpressionRepository.findByMemberAndPost(post, mergedMember).isPresent())
+            throw new AlreadyExistExpressionException(ErrorCode._400_ALREADY_EXIST_EXPRESSION);
+        PostExpression postExpression = PostExpression.createPostExpression(post, mergedMember, type);
+        entityManager.persist(postExpression);
     }
 
 }
