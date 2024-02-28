468292fda4aa010d6497b3b9db537575fcc35bae
5innim <bjm22247@gmail.com>
2024-02-28 14:43:35 +0900

    feat: Add logic for increasing views

--- src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeService.java
+++ src/main/java/com/innim/okkycopy/domain/board/knowledge/KnowledgeService.java
@@ -69,6 +69,7 @@ public class KnowledgeService {
                             .build()
             );
         }
+        knowledgePost.setViews(knowledgePost.getViews() + 1);
 
         return response;
     }
