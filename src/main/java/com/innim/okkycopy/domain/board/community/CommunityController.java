7bd6c35e11c9f22f65ed1ebfa58a4d038630bc25
5innim <bjm22247@gmail.com>
2024-04-01 14:35:30 +0900

    feat: Add CommunityController class and handler method for [post] /board/community/write

---
+++ src/main/java/com/innim/okkycopy/domain/board/community/CommunityController.java
@@ -0,0 +1,29 @@
+package com.innim.okkycopy.domain.board.community;
+
+import com.innim.okkycopy.domain.board.dto.request.write.PostRequest;
+import com.innim.okkycopy.global.auth.CustomUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board/community")
+@RequiredArgsConstructor
+public class CommunityController {
+
+    private final CommunityPostService communityPostService;
+
+    @PostMapping("/write")
+    public ResponseEntity<Object> communityPostAdd(@RequestBody @Valid PostRequest postRequest,
+        @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        communityPostService.addCommunityPost(postRequest, customUserDetails);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
