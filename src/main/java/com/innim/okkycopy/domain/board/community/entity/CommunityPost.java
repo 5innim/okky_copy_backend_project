46f2380f6b83d71641dd0331f9b26c50436eeca5
5innim <bjm22247@gmail.com>
2024-04-01 14:34:14 +0900

    feat: Add 'of' method at CommunityPost class

--- src/main/java/com/innim/okkycopy/domain/board/community/entity/CommunityPost.java
+++ src/main/java/com/innim/okkycopy/domain/board/community/entity/CommunityPost.java
@@ -1,13 +1,22 @@
 package com.innim.okkycopy.domain.board.community.entity;
 
+import com.innim.okkycopy.domain.board.dto.request.write.PostRequest;
+import com.innim.okkycopy.domain.board.dto.request.write.TagInfo;
 import com.innim.okkycopy.domain.board.entity.BoardTopic;
 import com.innim.okkycopy.domain.board.entity.Post;
+import com.innim.okkycopy.domain.board.entity.Tag;
+import com.innim.okkycopy.domain.member.entity.Member;
+import com.innim.okkycopy.global.error.ErrorCase;
+import com.innim.okkycopy.global.error.exception.StatusCode400Exception;
+import com.innim.okkycopy.global.error.exception.StatusCodeException;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -40,5 +49,33 @@ public class CommunityPost extends Post {
     private Integer views;
     @Column(nullable = false)
     private Integer comments;
+
+    public static CommunityPost of(PostRequest postRequest, BoardTopic boardTopic, Member member)
+        throws StatusCodeException {
+        if (!boardTopic.getBoardType().getName().equals("커뮤니티")) {
+            throw new StatusCode400Exception(ErrorCase._400_BAD_FORM_DATA);
+        }
+        CommunityPost communityPost = CommunityPost.builder()
+            .member(member)
+            .content(postRequest.getContent())
+            .title(postRequest.getTitle())
+            .lastUpdate(null)
+            .boardTopic(boardTopic)
+            .likes(0)
+            .hates(0)
+            .scraps(0)
+            .views(0)
+            .comments(0)
+            .build();
+
+        List<Tag> tags = new ArrayList<>();
+        for (TagInfo tag : postRequest.getTags()) {
+            tags.add(CommunityTag.of(communityPost, boardTopic, tag.getName()));
+        }
+        member.getPosts().add((Post) communityPost);
+        communityPost.setTags(tags);
+
+        return communityPost;
+    }
 
 }
