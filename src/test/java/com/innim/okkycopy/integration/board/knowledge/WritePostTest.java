02d25c682fd8c898a9aa8625e41785a50db2efa4
5innim <bjm22247@gmail.com>
2024-04-01 15:32:20 +0900

    test: Add test code for [post] /board/knowledge/write

--- src/test/java/com/innim/okkycopy/integration/board/knowledge/WritePostTest.java
+++ src/test/java/com/innim/okkycopy/integration/board/knowledge/WritePostTest.java
@@ -2,6 +2,7 @@ package com.innim.okkycopy.integration.board.knowledge;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.google.gson.Gson;
 import com.innim.okkycopy.domain.board.dto.request.write.PostRequest;
@@ -21,6 +22,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,9 +60,9 @@ public class WritePostTest {
 
     @Test
     @Transactional
-    void writeKnowledgePost() throws Exception {
+    void given_correctRequest_then_response201StatusCode() throws Exception {
         // given
-        PostRequest postRequest = writeRequest();
+        PostRequest postRequest = postRequest();
 
         // when
         MockHttpServletResponse response = mockMvc.perform(
@@ -75,23 +77,36 @@ public class WritePostTest {
     }
 
     @Test
-    void getKnowledgePost() throws Exception {
+    @Transactional
+    void given_notMatchedTopic_then_response400031() throws Exception {
         // given
-        // init_test_data procedure success
+        PostRequest notMatchedPostRequest = notMatchedPostRequest();
 
         // when
-        MockHttpServletResponse response = mockMvc.perform(
-            MockMvcRequestBuilders.get("/board/knowledge/posts/1")
-        ).andReturn().getResponse();
+        ResultActions resultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/board/knowledge/write")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(new Gson().toJson(notMatchedPostRequest))
+        );
 
         // then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        resultActions.andExpect(jsonPath("code").value("400031"));
     }
 
-    PostRequest writeRequest() {
+    PostRequest postRequest() {
         return PostRequest.builder()
             .title("test_title")
             .topic("Tech 뉴스")
+            .tags(Arrays.asList())
+            .content("test_content")
+            .build();
+    }
+
+    PostRequest notMatchedPostRequest() {
+        return PostRequest.builder()
+            .title("test_title")
+            .topic("기타")
             .tags(Arrays.asList())
             .content("test_content")
             .build();
