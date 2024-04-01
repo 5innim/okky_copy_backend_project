ffbb1f58be615394e20498a734a163c855f0e1ab
5innim <bjm22247@gmail.com>
2024-04-01 14:32:15 +0900

    feat: Add of method at CommunityTag class

--- src/main/java/com/innim/okkycopy/domain/board/community/entity/CommunityTag.java
+++ src/main/java/com/innim/okkycopy/domain/board/community/entity/CommunityTag.java
@@ -1,6 +1,7 @@
 package com.innim.okkycopy.domain.board.community.entity;
 
 import com.innim.okkycopy.domain.board.entity.BoardTopic;
+import com.innim.okkycopy.domain.board.entity.Post;
 import com.innim.okkycopy.domain.board.entity.Tag;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
@@ -21,4 +22,13 @@ public class CommunityTag extends Tag {
     @ManyToOne(optional = false)
     @JoinColumn(name = "topic_id")
     private BoardTopic boardTopic;
+
+    public static CommunityTag of(CommunityPost communityPost, BoardTopic boardTopic, String name) {
+        CommunityTag communityTag = new CommunityTag();
+        communityTag.setPost((Post) communityPost);
+        communityTag.setName(name);
+        communityTag.setBoardTopic(boardTopic);
+
+        return communityTag;
+    }
 }
