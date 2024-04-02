e41d9631a988e2b2d4d2c433061eb083d90d8482
5innim <bjm22247@gmail.com>
2024-04-02 12:26:28 +0900

    feat: Add 'of' method for CommunityPost class

--- src/main/java/com/innim/okkycopy/domain/board/dto/response/post/detail/PostDetailsResponse.java
+++ src/main/java/com/innim/okkycopy/domain/board/dto/response/post/detail/PostDetailsResponse.java
@@ -1,5 +1,6 @@
 package com.innim.okkycopy.domain.board.dto.response.post.detail;
 
+import com.innim.okkycopy.domain.board.community.entity.CommunityPost;
 import com.innim.okkycopy.domain.board.dto.response.post.WriterInfo;
 import com.innim.okkycopy.domain.board.entity.Tag;
 import com.innim.okkycopy.domain.board.knowledge.entity.KnowledgePost;
@@ -29,6 +30,7 @@ public class PostDetailsResponse {
     private RequesterInfo requesterInfo;
 
 
+    // TODO: should add case for another post domain at this method
     public static PostDetailsResponse of(KnowledgePost knowledgePost, Member member) {
         List<String> tags = new ArrayList<>();
         for (Tag tag : knowledgePost.getTags()) {
@@ -46,6 +48,26 @@ public class PostDetailsResponse {
             .likes(knowledgePost.getLikes())
             .hates(knowledgePost.getHates())
             .createdDate(knowledgePost.getCreatedDate())
+            .build();
+    }
+
+    public static PostDetailsResponse of(CommunityPost communityPost, Member member) {
+        List<String> tags = new ArrayList<>();
+        for (Tag tag : communityPost.getTags()) {
+            tags.add(tag.getName());
+        }
+
+        return PostDetailsResponse.builder()
+            .writerInfo((member == null) ? null : WriterInfo.builder()
+                .memberId(member.getMemberId())
+                .nickName(member.getNickname())
+                .profile(member.getProfile()).build())
+            .title(communityPost.getTitle())
+            .content(communityPost.getContent())
+            .tags(tags).views(communityPost.getViews())
+            .likes(communityPost.getLikes())
+            .hates(communityPost.getHates())
+            .createdDate(communityPost.getCreatedDate())
             .build();
     }
 
