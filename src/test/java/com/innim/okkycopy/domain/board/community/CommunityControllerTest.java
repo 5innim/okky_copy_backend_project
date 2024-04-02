604640e879fceb7531707d10d7a7fc74b7ee67d2
5innim <bjm22247@gmail.com>
2024-04-02 13:31:46 +0900

    test: Add test code for [get] /board/community/posts/{id}

--- src/test/java/com/innim/okkycopy/domain/board/community/CommunityControllerTest.java
+++ src/test/java/com/innim/okkycopy/domain/board/community/CommunityControllerTest.java
@@ -1,12 +1,15 @@
 package com.innim.okkycopy.domain.board.community;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
 import com.innim.okkycopy.common.WithMockCustomUserSecurityContextFactory;
 import com.innim.okkycopy.domain.board.dto.request.write.PostRequest;
+import com.innim.okkycopy.domain.board.dto.response.post.detail.PostDetailsResponse;
 import com.innim.okkycopy.global.auth.CustomUserDetails;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,7 +30,7 @@ public class CommunityControllerTest {
     @Test
     void writeKnowledgePostTest() {
         // given
-        PostRequest postRequest = writeRequest();
+        PostRequest postRequest = postRequest();
         CustomUserDetails customUserDetails = WithMockCustomUserSecurityContextFactory.customUserDetailsMock();
 
         // when
@@ -38,12 +41,36 @@ public class CommunityControllerTest {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
     }
 
-    PostRequest writeRequest() {
+    @Test
+    void getKnowledgePostTest() {
+        // given
+        long id = 1L;
+        PostDetailsResponse postDetailsResponse = postDetailRequest();
+        given(communityPostService.findCommunityPost(null, id)).willReturn(postDetailsResponse);
+
+        // when
+        ResponseEntity response = communityController.communityPostDetails(null, id);
+
+        // then
+        assertThat(response.getBody()).isEqualTo(postDetailsResponse);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    PostRequest postRequest() {
         return PostRequest.builder()
             .title("test_title")
             .topic("test_topic")
             .tags(Arrays.asList())
             .content("test_content")
+            .build();
+    }
+
+    PostDetailsResponse postDetailRequest() {
+        return PostDetailsResponse.builder()
+            .title("test_title")
+            .tags(Arrays.asList())
+            .content("test_content")
+            .createdDate(LocalDateTime.now())
             .build();
     }
 
