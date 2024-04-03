558ace6a3d1f2e1eaf5e1879c8d041262a4de28e
5innim <bjm22247@gmail.com>
2024-04-03 20:52:44 +0900

    feat: Add query with condition methods

--- src/main/java/com/innim/okkycopy/domain/board/community/CommunityPostRepository.java
+++ src/main/java/com/innim/okkycopy/domain/board/community/CommunityPostRepository.java
@@ -1,10 +1,21 @@
 package com.innim.okkycopy.domain.board.community;
 
 import com.innim.okkycopy.domain.board.community.entity.CommunityPost;
+import com.innim.okkycopy.domain.board.entity.BoardTopic;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long> {
 
     Optional<CommunityPost> findByPostId(long postId);
+
+    @Query("SELECT c FROM CommunityPost c WHERE c.boardTopic = :boardTopic AND c.title LIKE CONCAT('%', :keyword, '%')")
+    Page<CommunityPost> findByTopicId(@Param("boardTopic") BoardTopic boardTopic, String keyword, Pageable pageable);
+
+    @Query("SELECT c FROM CommunityPost c WHERE c.title LIKE CONCAT('%', :keyword, '%')")
+    Page<CommunityPost> findAllByKeywordAndPageable(String keyword, Pageable pageable);
 }
