f10c2833014dad9ae0cc1860042b9f6c6a00a630
5innim <bjm22247@gmail.com>
2024-04-03 12:37:40 +0900

    feat: Add handler method for [delete] /board/community/posts/{id}

--- src/main/java/com/innim/okkycopy/domain/board/community/CommunityController.java
+++ src/main/java/com/innim/okkycopy/domain/board/community/CommunityController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,14 @@ public class CommunityController {
         @RequestBody @Valid PostRequest updateRequest,
         @PathVariable("id") long id) {
         communityPostService.modifyCommunityPost(customUserDetails, updateRequest, id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Object> communityPostRemove(
+        @AuthenticationPrincipal CustomUserDetails customUserDetails,
+        @PathVariable("id") long id) {
+        communityPostService.removeCommunityPost(customUserDetails, id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
