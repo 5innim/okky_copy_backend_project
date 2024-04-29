d8cc891ceb4dc1db022ffbe7ae5a779bfbf64714
5innim <bjm22247@gmail.com>
2024-04-29 21:18:29 +0900

    feat: Init setting for oauth2Login

--- src/main/java/com/innim/okkycopy/global/config/SecurityConfig.java
+++ src/main/java/com/innim/okkycopy/global/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -45,6 +46,7 @@ public class SecurityConfig {
             .servletApi(AbstractHttpConfigurer::disable)
             .anonymous(AbstractHttpConfigurer::disable)
             .exceptionHandling(AbstractHttpConfigurer::disable)
+            .oauth2Login(Customizer.withDefaults())
             .authorizeHttpRequests(request -> {
                 request.requestMatchers(HttpMethod.POST,
                         "/board/knowledge/write",
