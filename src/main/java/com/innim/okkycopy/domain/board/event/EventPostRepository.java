73d9bd61535935323075e56518888ea8d8481778
5innim <bjm22247@gmail.com>
2024-04-29 21:47:16 +0900

    feat: Add EventPostRepository interface

---
+++ src/main/java/com/innim/okkycopy/domain/board/event/EventPostRepository.java
@@ -0,0 +1,21 @@
+package com.innim.okkycopy.domain.board.event;
+
+import com.innim.okkycopy.domain.board.entity.BoardTopic;
+import com.innim.okkycopy.domain.board.event.entity.EventPost;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface EventPostRepository extends JpaRepository<EventPost, Long> {
+    Optional<EventPost> findByPostId(long postId);
+
+    @Query("SELECT e FROM EventPost e WHERE e.boardTopic = :boardTopic AND e.title LIKE CONCAT('%', :keyword, '%')")
+    Page<EventPost> findByTopicId(@Param("boardTopic") BoardTopic boardTopic, String keyword, Pageable pageable);
+
+    @Query("SELECT e FROM EventPost e WHERE e.title LIKE CONCAT('%', :keyword, '%')")
+    Page<EventPost> findAllByKeywordAndPageable(String keyword, Pageable pageable);
+
+}
