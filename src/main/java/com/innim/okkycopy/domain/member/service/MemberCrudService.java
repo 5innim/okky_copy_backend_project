fa02ea5347c0a773bc597f86005ac08e3371f018
5innim <bjm22247@gmail.com>
2024-05-01 12:10:18 +0900

    feat: Add OkkyMemberService class

--- src/main/java/com/innim/okkycopy/domain/member/service/MemberCrudService.java
+++ src/main/java/com/innim/okkycopy/domain/member/service/MemberCrudService.java
@@ -1,54 +1,21 @@
 package com.innim.okkycopy.domain.member.service;
 
 import com.innim.okkycopy.domain.board.entity.Scrap;
-import com.innim.okkycopy.domain.member.MemberRepository;
-import com.innim.okkycopy.domain.member.dto.request.MemberRequest;
-import com.innim.okkycopy.domain.member.dto.response.MemberBriefResponse;
 import com.innim.okkycopy.domain.member.dto.response.MemberDetailsResponse;
 import com.innim.okkycopy.domain.member.entity.Member;
-import com.innim.okkycopy.global.error.ErrorCase;
-import com.innim.okkycopy.global.error.exception.StatusCode409Exception;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class MemberCrudService {
-
-    private final PasswordEncoder passwordEncoder;
-    private final MemberRepository memberRepository;
-
     @PersistenceContext
-    private EntityManager entityManager;
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
-    public MemberBriefResponse addMember(MemberRequest memberRequest) {
-
-        if (memberRepository.existsById(memberRequest.getId())) {
-            throw new StatusCode409Exception(ErrorCase._409_DUPLICATE_ID);
-        }
-
-        if (memberRepository.existsByEmail(memberRequest.getEmail())) {
-            throw new StatusCode409Exception(ErrorCase._409_DUPLICATE_EMAIL);
-        }
-
-        memberRequest.encodePassword(passwordEncoder);
-
-        Member member = Member.from(memberRequest);
-        memberRepository.save(member);
-
-        return MemberBriefResponse.from(member);
-    }
+    EntityManager entityManager;
 
     @Transactional(readOnly = true)
     public MemberDetailsResponse findMember(Member member) {
