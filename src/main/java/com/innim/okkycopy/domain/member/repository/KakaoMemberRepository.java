14a48afee31422cb40433f7e65324a613f540038
5innim <bjm22247@gmail.com>
2024-05-02 21:33:16 +0900

    feat: Fix wrong type in KakaoMemberRepository

--- src/main/java/com/innim/okkycopy/domain/member/repository/KakaoMemberRepository.java
+++ src/main/java/com/innim/okkycopy/domain/member/repository/KakaoMemberRepository.java
@@ -1,10 +1,9 @@
 package com.innim.okkycopy.domain.member.repository;
 
-import com.innim.okkycopy.domain.member.entity.GoogleMember;
 import com.innim.okkycopy.domain.member.entity.KakaoMember;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KakaoMemberRepository extends JpaRepository<KakaoMember, Long> {
-    Optional<GoogleMember> findByEmail(String email);
+    Optional<KakaoMember> findByEmail(String email);
 }
