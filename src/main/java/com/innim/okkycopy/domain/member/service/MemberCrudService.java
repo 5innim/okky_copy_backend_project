66c75586c339121f74a785ca35b9c8d1c83d9d1d
5innim <bjm22247@gmail.com>
2024-05-09 12:42:47 +0900

    feat: Add profile field and from method at MemberDetailsResponse dto class

--- src/main/java/com/innim/okkycopy/domain/member/service/MemberCrudService.java
+++ src/main/java/com/innim/okkycopy/domain/member/service/MemberCrudService.java
@@ -1,12 +1,9 @@
 package com.innim.okkycopy.domain.member.service;
 
-import com.innim.okkycopy.domain.board.entity.Scrap;
 import com.innim.okkycopy.domain.member.dto.response.MemberDetailsResponse;
 import com.innim.okkycopy.domain.member.entity.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,19 +18,7 @@ public class MemberCrudService {
     public MemberDetailsResponse findMember(Member member) {
         Member mergedMember = entityManager.merge(member);
 
-        List<Long> scrappedPostIdList = new ArrayList<>();
-        List<Scrap> scraps = mergedMember.getScrapList();
-        if (scraps != null) {
-            for (Scrap scrap : scraps) {
-                scrappedPostIdList.add(scrap.getPost().getPostId());
-            }
-        }
-
-        return MemberDetailsResponse.builder()
-            .memberId(mergedMember.getMemberId())
-            .nickname(mergedMember.getNickname())
-            .scrappedPost(scrappedPostIdList)
-            .build();
+        return MemberDetailsResponse.from(mergedMember);
     }
 
 }
