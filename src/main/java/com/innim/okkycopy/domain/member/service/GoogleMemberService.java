82354680cf2aed411f5bccc43d5022d6f53737ad
5innim <bjm22247@gmail.com>
2024-05-09 12:39:43 +0900

    feat: Add add{provider}Member method

--- src/main/java/com/innim/okkycopy/domain/member/service/GoogleMemberService.java
+++ src/main/java/com/innim/okkycopy/domain/member/service/GoogleMemberService.java
@@ -1,7 +1,14 @@
 package com.innim.okkycopy.domain.member.service;
 
+import com.innim.okkycopy.domain.member.dto.request.OAuthMemberRequest;
 import com.innim.okkycopy.domain.member.entity.GoogleMember;
 import com.innim.okkycopy.domain.member.repository.GoogleMemberRepository;
+import com.innim.okkycopy.global.auth.CustomOAuth2User;
+import com.innim.okkycopy.global.error.ErrorCase;
+import com.innim.okkycopy.global.error.exception.StatusCode400Exception;
+import com.innim.okkycopy.global.error.exception.StatusCode401Exception;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +21,30 @@ public class GoogleMemberService {
     private final GoogleMemberRepository googleMemberRepository;
 
     @Transactional(readOnly = true)
-    public GoogleMember findGoogleMember(String email) {
-        Optional<GoogleMember> googleMember = googleMemberRepository.findByEmail(email);
+    public GoogleMember findGoogleMember(String providerId) {
+        Optional<GoogleMember> googleMember = googleMemberRepository.findByProviderId(providerId);
         return googleMember.orElse(null);
+    }
+
+    @Transactional
+    public Long addGoogleMember(OAuthMemberRequest oAuthMemberRequest, String provider, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new StatusCode401Exception(ErrorCase._401_NOT_EXIST_SESSION);
+        }
+
+        Object value = session.getAttribute(oAuthMemberRequest.getKey());
+        if (value == null) {
+            throw new StatusCode400Exception(ErrorCase._400_BAD_FORM_DATA);
+        }
+
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) value;
+        if (!oAuth2User.getRegistrationId().equals(provider)) {
+            throw new StatusCode400Exception(ErrorCase._400_NO_ACCEPTABLE_PARAMETER);
+        }
+
+        GoogleMember member = googleMemberRepository.save(GoogleMember.of(oAuthMemberRequest, oAuth2User));
+        return member.getMemberId();
     }
 
 }
--- src/main/java/com/innim/okkycopy/domain/member/service/NaverMemberService.java
+++ src/main/java/com/innim/okkycopy/domain/member/service/NaverMemberService.java
@@ -1,7 +1,14 @@
 package com.innim.okkycopy.domain.member.service;
 
+import com.innim.okkycopy.domain.member.dto.request.OAuthMemberRequest;
 import com.innim.okkycopy.domain.member.entity.NaverMember;
 import com.innim.okkycopy.domain.member.repository.NaverMemberRepository;
+import com.innim.okkycopy.global.auth.CustomOAuth2User;
+import com.innim.okkycopy.global.error.ErrorCase;
+import com.innim.okkycopy.global.error.exception.StatusCode400Exception;
+import com.innim.okkycopy.global.error.exception.StatusCode401Exception;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +21,30 @@ public class NaverMemberService {
     private final NaverMemberRepository naverMemberRepository;
 
     @Transactional(readOnly = true)
-    public NaverMember findNaverMember(String email) {
-        Optional<NaverMember> naverMember = naverMemberRepository.findByEmail(email);
+    public NaverMember findNaverMember(String providerId) {
+        Optional<NaverMember> naverMember = naverMemberRepository.findByProviderId(providerId);
         return naverMember.orElse(null);
+    }
+
+    @Transactional
+    public Long addNaverMember(OAuthMemberRequest oAuthMemberRequest, String provider, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new StatusCode401Exception(ErrorCase._401_NOT_EXIST_SESSION);
+        }
+
+        Object value = session.getAttribute(oAuthMemberRequest.getKey());
+        if (value == null) {
+            throw new StatusCode400Exception(ErrorCase._400_BAD_FORM_DATA);
+        }
+
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) value;
+        if (!oAuth2User.getRegistrationId().equals(provider)) {
+            throw new StatusCode400Exception(ErrorCase._400_NO_ACCEPTABLE_PARAMETER);
+        }
+
+        NaverMember member = naverMemberRepository.save(NaverMember.of(oAuthMemberRequest, oAuth2User));
+        return member.getMemberId();
     }
 
 }
