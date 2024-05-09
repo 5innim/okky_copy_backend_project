eaddde34317b6fb54b4024c1b3b5b52c39abdae3
5innim <bjm22247@gmail.com>
2024-05-09 12:43:07 +0900

    feat: Add OAuthMemberRequest dto class

---
+++ src/main/java/com/innim/okkycopy/domain/member/dto/request/OAuthMemberRequest.java
@@ -0,0 +1,18 @@
+package com.innim.okkycopy.domain.member.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OAuthMemberRequest {
+
+    private String key;
+    @Pattern(regexp = "^[A-Za-z가-힣0-9]{2,20}$")
+    private String nickname;
+    private String profile;
+    private boolean emailCheck;
+}
