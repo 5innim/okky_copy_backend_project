4e0ffb8053b8f6e84f797bdb1ac554df1168e576
5innim <bjm22247@gmail.com>
2024-05-12 12:10:58 +0900

    feat: Fix logic for 'of' method at KakaoMember and NaverMember entity

--- src/main/java/com/innim/okkycopy/domain/member/entity/KakaoMember.java
+++ src/main/java/com/innim/okkycopy/domain/member/entity/KakaoMember.java
@@ -3,10 +3,13 @@ package com.innim.okkycopy.domain.member.entity;
 import com.innim.okkycopy.domain.member.dto.request.OAuthMemberRequest;
 import com.innim.okkycopy.global.auth.CustomOAuth2User;
 import com.innim.okkycopy.global.auth.enums.Role;
+import com.innim.okkycopy.global.error.ErrorCase;
+import com.innim.okkycopy.global.error.exception.StatusCode500Exception;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.util.LinkedHashMap;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -27,10 +30,17 @@ public class KakaoMember extends Member {
     private String email;
 
     public static KakaoMember of(OAuthMemberRequest request, CustomOAuth2User oAuth2User) {
+        LinkedHashMap<String, String> properties = oAuth2User.getAttribute("properties");
+        LinkedHashMap<String, String> kakaoAccount = oAuth2User.getAttribute("kakao_account");
+        if (properties == null || kakaoAccount == null) {
+            throw new StatusCode500Exception(ErrorCase._500_NULL_PROPERTY);
+        }
+
         return KakaoMember.builder()
             .providerId(oAuth2User.getName())
-            .email(oAuth2User.getAttribute("email"))
-            .name(oAuth2User.getAttribute("name"))
+            .email(kakaoAccount.get("email"))
+//            .name(properties.get("name")) TODO "After authenticate business app at kakao, can use name property"
+            .name(request.getNickname())
             .nickname(request.getNickname())
             .emailCheck(request.isEmailCheck())
             .role(Role.USER)
--- src/main/java/com/innim/okkycopy/domain/member/entity/NaverMember.java
+++ src/main/java/com/innim/okkycopy/domain/member/entity/NaverMember.java
@@ -3,10 +3,13 @@ package com.innim.okkycopy.domain.member.entity;
 import com.innim.okkycopy.domain.member.dto.request.OAuthMemberRequest;
 import com.innim.okkycopy.global.auth.CustomOAuth2User;
 import com.innim.okkycopy.global.auth.enums.Role;
+import com.innim.okkycopy.global.error.ErrorCase;
+import com.innim.okkycopy.global.error.exception.StatusCode500Exception;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import java.util.LinkedHashMap;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -27,10 +30,20 @@ public class NaverMember extends Member{
     private String email;
 
     public static NaverMember of(OAuthMemberRequest request, CustomOAuth2User oAuth2User) {
+        LinkedHashMap<String, String> map = null;
+        try {
+            map = oAuth2User.getAttribute("response");
+            if (map == null) {
+                throw new StatusCode500Exception(ErrorCase._500_NULL_PROPERTY);
+            }
+        } catch (StatusCode500Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return NaverMember.builder()
-            .providerId(oAuth2User.getName())
-            .email(oAuth2User.getAttribute("email"))
-            .name(oAuth2User.getAttribute("name"))
+            .providerId(map.get("id"))
+            .email(map.get("email"))
+            .name(map.get("name"))
             .nickname(request.getNickname())
             .emailCheck(request.isEmailCheck())
             .role(Role.USER)
