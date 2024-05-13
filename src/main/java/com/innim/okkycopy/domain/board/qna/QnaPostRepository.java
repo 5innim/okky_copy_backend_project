d493118a831bb8a7243427b208198dba945c4960
5innim <bjm22247@gmail.com>
2024-05-13 17:14:20 +0900

    feat: Add QnaPostRepository interface

---
+++ src/main/java/com/innim/okkycopy/domain/board/qna/QnaPostRepository.java
@@ -0,0 +1,20 @@
+package com.innim.okkycopy.domain.board.qna;
+
+import com.innim.okkycopy.domain.board.entity.BoardTopic;
+import com.innim.okkycopy.domain.board.qna.entity.QnaPost;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface QnaPostRepository extends JpaRepository<QnaPost, Long> {
+    Optional<QnaPost> findByPostId(long postId);
+
+    @Query("SELECT q FROM QnaPost q WHERE q.boardTopic = :boardTopic AND q.title LIKE CONCAT('%', :keyword, '%')")
+    Page<QnaPost> findByTopicId(@Param("boardTopic") BoardTopic boardTopic, String keyword, Pageable pageable);
+
+    @Query("SELECT q FROM QnaPost q WHERE q.title LIKE CONCAT('%', :keyword, '%')")
+    Page<QnaPost> findAllByKeywordAndPageable(String keyword, Pageable pageable);
+}
