8aa133c4afdf1a64d697ad630ee1889eee46d7bb
5innim <bjm22247@gmail.com>
2024-05-22 10:53:42 +0900

    feat: Add MemberModifyRequest dto class

---
+++ src/main/java/com/innim/okkycopy/domain/member/dto/request/MemberModifyRequest.java
@@ -0,0 +1,15 @@
+package com.innim.okkycopy.domain.member.dto.request;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MemberModifyRequest {
+    @Pattern(regexp = "^[A-Za-z가-힣]{2,20}$")
+    private String name;
+    @Pattern(regexp = "^[A-Za-z가-힣0-9]{2,20}$")
+    private String nickname;
+    private String profile;
+}
