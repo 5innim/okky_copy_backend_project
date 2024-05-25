d14a77efa1957f3dde388c3ecabd29973a7acf79
5innim <bjm22247@gmail.com>
2024-05-25 16:57:38 +0900

    feat: Add EmailAuthenticateValue class

---
+++ src/main/java/com/innim/okkycopy/global/util/email/EmailAuthenticateValue.java
@@ -0,0 +1,13 @@
+package com.innim.okkycopy.global.util.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class EmailAuthenticateValue {
+    private Long memberId;
+    private String email;
+
+
+}
