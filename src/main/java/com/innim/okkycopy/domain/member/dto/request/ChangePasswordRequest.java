b3887135c631955e5b9677dfa771399b19446c46
5innim <bjm22247@gmail.com>
2024-06-02 22:20:00 +0900

    feat: Add constructors and builder

--- src/main/java/com/innim/okkycopy/domain/member/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/innim/okkycopy/domain/member/dto/request/ChangePasswordRequest.java
@@ -1,11 +1,17 @@
 package com.innim.okkycopy.domain.member.dto.request;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChangePasswordRequest {
     @Pattern(regexp = "^(?=\\S*[0-9])(?=\\S*[a-zA-Z])\\S{6,20}$")
     private String oldPassword;
