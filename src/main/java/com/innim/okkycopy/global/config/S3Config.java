6e4f9f731d5cd6ddba1ba8ba88f7adbe0d2967b7
5innim <bjm22247@gmail.com>
2024-06-19 17:04:13 +0900

    feat: Update credentials and adjust jasypt

--- src/main/java/com/innim/okkycopy/global/config/S3Config.java
+++ src/main/java/com/innim/okkycopy/global/config/S3Config.java
@@ -11,12 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("#{environment['cloud.aws.credentials.access-key']}")
     private String accessKey;
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("#{environment['cloud.aws.credentials.secret-key']}")
     private String secretKey;
-    @Value("${cloud.aws.region.static}")
+    @Value("#{environment['cloud.aws.region.static']}")
     private String region;
+
+
 
     @Bean
     public AmazonS3Client amazonS3Client() {
