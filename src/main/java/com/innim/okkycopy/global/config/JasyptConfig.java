74b7b4dac18696d48155e4eb79054ca497c984ed
5innim <bjm22247@gmail.com>
2024-06-21 11:24:50 +0900

    feat: Config for dev environment

--- src/main/java/com/innim/okkycopy/global/config/JasyptConfig.java
+++ src/main/java/com/innim/okkycopy/global/config/JasyptConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"local", "test"})
+@Profile({"local", "test", "dev"})
 public class JasyptConfig {
 
     @Value("#{environment['jasypt.encryptor.key']}")
