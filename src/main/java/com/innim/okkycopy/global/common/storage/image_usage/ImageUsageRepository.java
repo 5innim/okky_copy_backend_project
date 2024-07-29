e85fba2a873907685e7a0a8300054fdae896bb5f
5innim <bjm22247@gmail.com>
2024-07-29 17:02:28 +0900

    feat: Add methods at ImageUsageRepository interface

--- src/main/java/com/innim/okkycopy/global/common/storage/image_usage/ImageUsageRepository.java
+++ src/main/java/com/innim/okkycopy/global/common/storage/image_usage/ImageUsageRepository.java
@@ -1,10 +1,19 @@
 package com.innim.okkycopy.global.common.storage.image_usage;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ImageUsageRepository extends JpaRepository<ImageUsage, Long> {
 
     Optional<ImageUsage> findByImageUsageId(long imageUsageId);
+
+    @Query("SELECT i from ImageUsage i WHERE i.isInUse = :isInUse AND i.createdDate < :date")
+    List<ImageUsage> findByIsInUseAndCreatedDate(boolean isInUse, LocalDateTime date);
+
+    @Query("DELETE from ImageUsage i WHERE i.isInUse = :isInUse AND i.createdDate < :date")
+    void deleteByIsInUseAndCreatedDate(boolean isInUse, LocalDateTime date);
 
 }
