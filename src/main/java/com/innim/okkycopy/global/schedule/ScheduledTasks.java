475026a5500345668d06e596f75e4ab305e4d4f2
5innim <bjm22247@gmail.com>
2024-07-29 17:03:42 +0900

    feat: Add schedule for remove image from S3 storage

---
+++ src/main/java/com/innim/okkycopy/global/schedule/ScheduledTasks.java
@@ -0,0 +1,37 @@
+package com.innim.okkycopy.global.schedule;
+
+import com.innim.okkycopy.global.common.storage.S3Uploader;
+import com.innim.okkycopy.global.common.storage.image_usage.ImageUsage;
+import com.innim.okkycopy.global.common.storage.image_usage.ImageUsageService;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ScheduledTasks {
+
+    private final ImageUsageService imageUsageService;
+    private final S3Uploader s3Uploader;
+
+    @Scheduled(cron = "0 35 0 * * *")
+    public void removeUnusedImageFromS3() {
+        log.info("schedule start: removeUnusedImageFromS3");
+        Date date = new Date();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        LocalDateTime midnightTime = localDateTime.withHour(0).withMinute(0).withSecond(0).withNano(0);
+
+        List<ImageUsage> imageUsages = imageUsageService.findImageUsagesByIsInUseAndCreatedDate(false, midnightTime);
+        for (ImageUsage imageUsage : imageUsages) {
+            s3Uploader.deleteFileFromS3(imageUsage.getImageUsageId().toString());
+        }
+
+        imageUsageService.removeImageUsagesByIsInUseAndCreatedDate(false, midnightTime);
+    }
+}
