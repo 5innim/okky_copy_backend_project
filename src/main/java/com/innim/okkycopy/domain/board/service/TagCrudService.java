62641c06866763c1336a51b1d8c0eac3d29cfee8
5innim <bim22247@gmail.com>
2024-08-03 13:56:57 +0900

    feat: Extract methods about top tag and tag from WeeklyScheduleService class

---
+++ src/main/java/com/innim/okkycopy/domain/board/service/TagCrudService.java
@@ -0,0 +1,23 @@
+package com.innim.okkycopy.domain.board.service;
+
+import com.innim.okkycopy.domain.board.dto.result.TagCntQueryResult;
+import com.innim.okkycopy.domain.board.repository.TagRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TagCrudService {
+
+    private final TagRepository tagRepository;
+
+    @Transactional(readOnly = true)
+    public List<TagCntQueryResult> findTopTagsByCreatedDateRange(LocalDateTime startDate, LocalDateTime endDate,
+        int limit) {
+        return tagRepository.findTopTagsByCreatedDateRange(startDate, endDate, PageRequest.of(0, limit));
+    }
+}
---
+++ src/main/java/com/innim/okkycopy/domain/board/service/TopTagService.java
@@ -0,0 +1,34 @@
+package com.innim.okkycopy.domain.board.service;
+
+import com.innim.okkycopy.domain.board.dto.response.top_tag.TopTagListResponse;
+import com.innim.okkycopy.domain.board.entity.TopTag;
+import com.innim.okkycopy.domain.board.repository.TopTagRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TopTagService {
+
+    private final TopTagRepository topTagRepository;
+
+    @Transactional
+    public void removeAllTopTag() {
+        topTagRepository.deleteAll();
+    }
+
+
+    @Transactional
+    public void addTopTag(int rank, String name, int creates) {
+        TopTag topTag = new TopTag(rank, name, creates);
+        topTagRepository.save(topTag);
+    }
+
+    @Transactional(readOnly = true)
+    public TopTagListResponse findAll() {
+        List<TopTag> topTagList = topTagRepository.findAll();
+        return TopTagListResponse.from(topTagList);
+    }
+}
