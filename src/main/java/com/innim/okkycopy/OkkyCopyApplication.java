e7afc4f648ede43d2ecfa355e83b7e332cfb8abb
5innim <bjm22247@gmail.com>
2024-09-28 21:53:09 +0900

    refactor: Add @PropertySources for reading added property files

--- src/main/java/com/innim/okkycopy/OkkyCopyApplication.java
+++ src/main/java/com/innim/okkycopy/OkkyCopyApplication.java
@@ -4,11 +4,16 @@ import com.innim.okkycopy.global.common.YamlLoadFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@PropertySource(value = "classpath:/config/jwt.yml", factory = YamlLoadFactory.class)
+@PropertySources({
+    @PropertySource(value = "classpath:/config/jwt.yml", factory = YamlLoadFactory.class),
+    @PropertySource(value = "classpath:/config/cloud.yml", factory = YamlLoadFactory.class),
+    @PropertySource(value = "classpath:/config/encrypt.yml", factory = YamlLoadFactory.class)
+})
 public class OkkyCopyApplication {
 
     public static void main(String[] args) {
